9636ed120dec499ad1ddc3f49470328904518ed6
nkempetra <uzomapeternkemdirim@gmail.com>
2023-03-24 05:54:03 -0700

    0x04

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,10 +1,12 @@
 #include "main.h"
 
-void print_numbers(void);
+void print_numbers(void)
 {
-	int num = 0;
-	for (num = 0; num <= 9; num++)
-	putchar(num);
-	
-	return (0)
+int number = 48;
+for (number = 48; number < 58; number++)
+{
+_putchar(number);
 }
+_putchar(10);
+}
+
