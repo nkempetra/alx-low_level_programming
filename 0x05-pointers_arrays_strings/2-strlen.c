0df61f4d64dbf1b5f1ec7a56d1529d0f3a56483b
nkempetra <uzomapeternkemdirim@gmail.com>
2023-03-27 22:12:36 +0000

    0x05

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+int _strlen(char *s);
+{
+    char *str;
+    int len;
+
+    str = "My first strlen!";
+    len = _strlen(str);
+    printf("%d\n", len);
+    return (0);
+}
