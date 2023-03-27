4ef2b508d84322437dc125c72d847f0d2c23ac8e
nkempetra <uzomapeternkemdirim@gmail.com>
2023-03-27 23:30:16 +0000

    0x05

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,13 +1,20 @@
 #include "main.h"
-#include <stdio.h>
+
+/**
+ *_strlen -length of a string
+ *@s: A pointer to an int that will be changed/updated
+ *
+ *Return: void that means our answer is correct
+ */
 
 int _strlen(char *s)
 {
-    char *str;
-    int len;
+int u;
 
-    str = "My first strlen!";
-    len = _strlen(str);
-    printf("%d\n", len);
-    return (0);
+u = 0;
+while (s[u] != '\0')
+{
+u++;
+}
+return (u);
 }
