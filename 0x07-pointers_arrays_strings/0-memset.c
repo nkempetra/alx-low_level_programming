adbcbba806199143108e5cd3d1e00896aa65a42d
nkempetra <uzomapeternkemdirim@gmail.com>
2023-04-03 23:55:58 +0000

    a function that fills memory with a constant byte

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * simple_print_buffer - prints buffer in hexa
+ * @buffer: the address of memory to print
+ * @size: the size of the memory to print
+ *
+ * Return: Nothing.
+ */
+
+char *_memset(char *s, char b, unsigned int n);
+{
+	while (n)
+	{
+		s[n-1] = b;
+		n--;
+	}
+	return(s);
+}
