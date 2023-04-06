54dfa90c4e43cd221096eb9a92bc3291b3e03c20
nkempetra <uzomapeternkemdirim@gmail.com>
2023-04-06 03:20:07 +0000

    0x08.5

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -2,20 +2,20 @@
 
 /**
  * int _pow_recursion - gives the value of x raised to the power of y
- * @x: the number to be raised to a power
- * @y: the power number
- * returns: the value of the number raised to power
+ * @x: the number to be raised to power y
+ * @y: the power number of x
+ * returns: the value of x raised to power y
  * */
 
 int _pow_recursion(int x, int y);
 {
 	if (y < 0)
 	{
-		return -1;
+		return (-1);
 	}
 	else if (y == 0)
 	{
-		return 1;
+		return (1);
 	}
 	else
 	{
