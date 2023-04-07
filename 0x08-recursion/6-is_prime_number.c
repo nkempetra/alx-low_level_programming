b4b9ccf178d5f81f6239323f613d366b6f4108ac
nkempetra <uzomapeternkemdirim@gmail.com>
2023-04-07 22:24:03 +0000

    0x08

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -2,11 +2,17 @@
 
 int is_prime_number(int n)
 {
-	for(int i = 2; i <= n / 2; i++)
-{
-	if(n % i == 0)
-		return 0;
-}
-
-return 1;
+    int i;
+    if (n < 2) 
+    { // A prime number must be greater than 1
+        return 0;
+    }
+    for (i = 2; i*i <= n; i++) 
+    {
+        if (n % i == 0) 
+	{ // If n is divisible by i, then it's not a prime number
+            return 0;
+        }
+    }
+    return (1);
 }
