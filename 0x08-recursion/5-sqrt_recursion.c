1d0fdfc8d8758f6257dbbbbadf8995deeaef0cff
nkempetra <uzomapeternkemdirim@gmail.com>
2023-04-07 06:57:41 +0000

    0x08.5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -9,20 +9,24 @@
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
-	{
-		return -1;
-	}
-	if (n == 0 || n == 1)
-	{
+
+		return (-1);
+
+	else if (n == 0 || n == 1)
+
 		return(1);
-	}
 
-	int i = 1;
+	else
 
-	while (i <= n)
-	{
-		i++;
-		result = i * i;
-	}
-	return i - 1;
+		return (_sqrt_recursion(n-1)+n-1);
+}
+
+int _sqrt_recursion(int n, int i)
+{
+	if ((i * i) > n)
+		return (-1);
+	else if ((i * i) == n)
+		return (i);
+	else
+		return (_sqrt_recursion(n, i + 1));
 }
