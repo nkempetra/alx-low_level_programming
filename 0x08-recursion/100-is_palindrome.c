b59fbf2a1a899c4d86294e672faf38f24bf056d1
nkempetra <uzomapeternkemdirim@gmail.com>
2023-04-10 09:21:04 +0000

    0x08

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -36,4 +36,22 @@ len += find_strlen(s + len);
 return (len);
 }
 
+/**
+ * check_palindrome - Checks if a string is a palindrome.
+ * @s: string to check.
+ * @len: length of s.
+ * @i: current index of iteration.
+ *
+ * Return: 1 If string is a palindrome.
+ *         0 If string is not a palindrome.
+ */
+int check_palindrome(char *s, int len, int i)
+{
+	if (s[i] == s[len / 2])
+		return (1);
 
+	if (s[i] == s[len - i - 1])
+		return (check_palindrome(s, len, i + 1));
+
+	return (0);
+}
