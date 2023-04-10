cc6a83bdd84c3eea61f240883f4f7fc9f4e9cd96
nkempetra <uzomapeternkemdirim@gmail.com>
2023-04-10 12:30:57 +0000

    0x01A

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -9,8 +9,8 @@
  * Return: Always 0 (Success) 
  */
 
-int main(int argc, char **argv)
+int main(int argc, char *argv)
 {
-	printf("%d\n", argc);
-	return 0;
+	printf("%d\n", argc - 1);
+	return (0);
 }
