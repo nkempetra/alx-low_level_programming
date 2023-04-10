9be88ddc0f371bafcba06f9d2af65c07dd302546
nkempetra <uzomapeternkemdirim@gmail.com>
2023-04-10 11:42:08 +0000

    0x01A

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/*
+ * main - prints name of file.
+ * @argc: argument count.
+ * @argv: argument content.
+ */
+
+int main(int argc, char **argv)
+{
+	printf("%s/n", argv[0]);
+	return 0;
+}
+
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,7 @@
+#ifndef MYHEADERFILE_H
+#define MYHEADERFILE_H
+
+int _putchar(char c);
+int print_name(int argc, char *argv[]);
+
+#endif
