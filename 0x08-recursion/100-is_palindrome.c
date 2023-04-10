4f2d90f6b8acc1e19cefa8e02492491ec2b90d6f
nkempetra <uzomapeternkemdirim@gmail.com>
2023-04-10 08:56:05 +0000

    0x08

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -3,24 +3,24 @@
 /**
  * is_palindrome - checks if a given string is a palindrome
  * @s: string to check
- * Return: 1 if the given string is a palindrome, 0 otherwise
+ * Return: 1 if string is a palindrome, 0 if string is not a palindrom
 */
 
 int is_palindrome(char *s)
 {
-int index = 0;
+int i = 0;
 int len = find_strlen(s);
 
 if (!(*s))
 	return (1);
 
-return (check_palindrome(s, len, index));
+return (check_palindrome(s, len, i));
 }
 
 /**
  * find_strlen - Returns the length of a string.
- * @s: The string to be measured.
- * Return: The length of the string.
+ * @s: string
+ * Return: string length.
 */
 
 int find_strlen(char *s)
