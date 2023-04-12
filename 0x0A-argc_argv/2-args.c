d878332e7c53696987f07356a34688a16c24cb26
nkempetra <uzomapeternkemdirim@gmail.com>
2023-04-12 17:15:57 +0000

    fix

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -9,10 +9,9 @@
  * return: 0.
  */
 
-int main(int argc, char *argv[])
+int main(int argc, char __attribute__((__unused__)) *argv[])
 {
 	int i;
-	(void) argc;
 	for (i = 0; i < argc; i++)
 	{
 		printf("%s\n", argv[i]);
