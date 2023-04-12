69e56694ba6fbc1d35d72a2f64c15e578a894a75
nkempetra <uzomapeternkemdirim@gmail.com>
2023-04-12 16:48:04 +0000

    fix

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -10,11 +10,7 @@
 
 int main(int argc, char* argv[])
 {
-        int i;
         (void) argv;
-        for (i = 0; i < argc; i++)
-        {
-                printf("%d\n", argc);
-        }
-        return 0;
+        printf("%d\n", argc - 1);
+       	return 0;
 }    
