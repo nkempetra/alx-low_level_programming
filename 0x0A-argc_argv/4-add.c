79739920a769096e4a442ea22071dfdd351194e9
nkempetra <uzomapeternkemdirim@gmail.com>
2023-04-12 11:47:50 +0000

    0x0a

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,50 +1,26 @@
 #include "main.h"
 
-/**
- * main - prints the number of arguments passed.
- * @argc: number of arguments passed to the program
- * @argv: an array of strings representing the arguments 
- *
- * Return: 0.  
- */
-
-int main(int argc, char* argv[])
+int main(int argc, char *argv[]) 
 {
-	int add = 0;
-	int i, x;
-
-	if (argc == 1)
+	int sum = 0;
+	if (argc == 1) 
 	{
-		printf("0\n");
-		return 1;
-	}
-
-	{
-		for (i = 1; i < argc; i++)
-		{
-			for (int j = 0; argv[i][j] != '\0'; j++) 
-			{
-				if (argv[i][j] < '0' || argv[i][j] > '9')
-				{
-					printf("Error\n");
-					return 1;
-				}
-                        }
-                }
-
-		{
-			x = strtol(argv[i], NULL, 10);
-			add = add + x;
-		       
-			if (x < 0 || x > 9)
-			{
-                        	printf("Error\n");
-                        	return 1;
-                	}
-		}
-
-		printf("%d\n", add);
-
+		printf("%d\n", sum);
 		return 0;
 	}
+	for (int i = 1; i < argc; i++) 
+	{
+		char *num = argv[i];
+		for (int j = 0; num[j] != '\0'; j++) 
+		{
+			if (!isdigit(num[j])) 
+			{
+				printf("Error\n");
+				return 1;
+			}
+		}
+		sum += atoi(num);
+	}
+	printf("%d\n", sum);
+	return 0;
 }
