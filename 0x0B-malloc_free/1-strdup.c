086efad284914486fdc9626a22cd3574eb3966f4
nkempetra <uzomapeternkemdirim@gmail.com>
2023-04-13 00:37:13 +0000

    0x0B

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -14,12 +14,12 @@ char *_strdup(char *str)
 	char *copy;
 
 	if (str == NULL)
-		return NULL;
+		return (NULL);
 	copy = malloc(strlen(str) + 1);
 
 	if (copy == NULL)
-		return NULL;
+		return (NULL);
 
 	strcpy(copy, str);
-	return copy;
+	return (copy);
 }
