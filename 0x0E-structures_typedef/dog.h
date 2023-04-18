1258c44b50da3af4a191d4e9d0d31d4d9786da81
nkempetra <uzomapeternkemdirim@gmail.com>
2023-04-18 00:35:17 +0000

    0x0E 1

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,22 +1,35 @@
-#include "dog.h"
+#ifndef DOG_H
+#define DOG_H
+
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
-* init_dog - inits a variable of
-*type dog
-*@d: dog identification
+*struct dog - a new type of data
+*representing a dog
 *@name: name of dog
 *@age: age of dog
 *@owner: owner's name
 */
-
 struct dog
 {
-    char *name;
-    float age;
-    char *owner;
+char *name;
+float age;
+char *owner;
 };
 
-void main()
-{
-    struct dog my_dog = { 'name', 40, 'owner'};
-}
+
+/**
+*dog_t - Typedef for struct dog
+*/
+typedef struct dog dog_t;
+
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+int _strlen(char *str);
+char *_strcopy(char *dest, char *src);
+
+#endif
