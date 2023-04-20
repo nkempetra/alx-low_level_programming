4913d585ec6ad88356dbf522de5868929db788e3
nkempetra <uzomapeternkemdirim@gmail.com>
2023-04-20 23:43:06 +0000

    sum all

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -13,15 +13,14 @@ int sum_them_all(const unsigned int n, ...)
 	int i, sum;
 
 	if (n == 0)
-                return 0;
+		return (0);
 
-
-	va_start (ap, n);
+	va_start(ap, n);
 
 	sum = 0;
 	for (i = 0; i < n; i++)
-		sum += va_arg (ap, int);
+		sum += va_arg(ap, int);
 
-	va_end (ap);
-	return sum;
+	va_end(ap);
+	return (sum);
 }
