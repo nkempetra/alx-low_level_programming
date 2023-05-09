b065143ba60d754606602768bc9f35a87955fe31
nkempetra <uzomapeternkemdirim@gmail.com>
2023-05-09 01:39:41 +0000

    1st

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,39 +1,49 @@
 #include "main.h"
 
-ssize_t read_textfile(const char *filename, size_t letters) 
+/**
+ * read_textfile - Reads a text file and prints it to POSIX stdout.
+ * @filename: A pointer to the name of the file.
+ * @letters: The number of letters the function should read and print.
+ *
+ * Return: If the function fails or filename is NULL - 0.
+ *         Otherwise - the actual number of bytes the function can read and print.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
 {
-	if (!filename) 
-	{
-		return 0;
-	}
+	if (!filename)
+		return (0);
 
-	FILE *file = fopen(filename, "r");
-	if (!file) {
-		return 0;
-	}
+	int file = open(filename, O_RDONLY);
+
+	if (file == -1)
+		return (0);
 
 	char *buffer = malloc(sizeof(char) * (letters + 1));
-	if (!buffer) {
-		fclose(file);
-		return 0;
+
+	if (!buffer)
+	{
+		close(file);
+		return (0);
 	}
 
-	ssize_t read_bytes = fread(buffer, sizeof(char), letters, file);
-	if (ferror(file)) {
+	ssize_t read_bytes = read(file, buffer, letters);
+
+	if (read_bytes == -1)
+	{
 		free(buffer);
-		fclose(file);
-		return 0;
+		close(file);
+		return (0);
 	}
 
 	buffer[read_bytes] = '\0';
 
-	if (write(STDOUT_FILENO, buffer, read_bytes) != read_bytes) {
-		free(buffer);
-		fclose(file);
-		return 0;
-	}
+	ssize_t bytes_written = write(STDOUT_FILENO, buffer, read_bytes);
 
 	free(buffer);
-	fclose(file);
-	return read_bytes;
+	close(file);
+
+	if (bytes_written != read_bytes)
+		return (0);
+
+	return (read_bytes);
 }
