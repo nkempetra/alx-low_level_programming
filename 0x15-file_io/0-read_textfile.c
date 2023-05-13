cbe41bfc080433e4ec52017d212ef603b90bf4bd
nkempetra <uzomapeternkemdirim@gmail.com>
2023-05-13 08:14:08 +0000

    resolve

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -6,19 +6,23 @@
  * @letters: The number of letters the function should read and print.
  *
  * Return: If the function fails or filename is NULL - 0.
- *         Otherwise - the actual number of bytes the function can read and print.
+ * Otherwise - the actual number of bytes
+ * the function can read and print.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
+	char *buffer; 
+	int file, read_bytes, bytes_written;
+
 	if (!filename)
 		return (0);
 
-	int file = open(filename, O_RDONLY);
+	file = open(filename, O_RDONLY);
 
 	if (file == -1)
 		return (0);
 
-	char *buffer = malloc(sizeof(char) * (letters + 1));
+	buffer = (char *) malloc(sizeof(char) * (letters + 1));
 
 	if (!buffer)
 	{
@@ -26,7 +30,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	}
 
-	ssize_t read_bytes = read(file, buffer, letters);
+	read_bytes = read(file, buffer, letters);
 
 	if (read_bytes == -1)
 	{
@@ -37,7 +41,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	buffer[read_bytes] = '\0';
 
-	ssize_t bytes_written = write(STDOUT_FILENO, buffer, read_bytes);
+	bytes_written = write(STDOUT_FILENO, buffer, read_bytes);
 
 	free(buffer);
 	close(file);
