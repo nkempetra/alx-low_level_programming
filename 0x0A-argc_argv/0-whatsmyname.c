595bbe308d3f45fec6a423faadcd1438f31100ec
itgeekng <itgeekng@gmail.com>
2023-06-09 20:42:58 +0000

    Fix 0x0A

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,7 +4,7 @@
  * main - print name of program.
  * @argc: number of arguments passed to the program.
  * @argv: an array of strings representing the arguments.
- * return: 0 always
+ * Return: 0 always
  */
 
 int main(int __attribute__((__unused__))argc, char *argv[])
