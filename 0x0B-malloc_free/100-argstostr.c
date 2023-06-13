ab3ddcd726c071610ff35af4c050ef60acb1b976
itgeekng <itgeekng@gmail.com>
2023-06-13 19:48:40 +0000

    0x0B

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,59 @@
+#include "main.h"
+
+/**
+ * argstostr - Concatenates all arguments of the program into a string
+ * arguments are separated by a new line in the string.
+ * @ac: The number of arguments passed to the program.
+ * @av: An array of pointers to the arguments.
+ *
+ * Return: If ac == 0, av == NULL, or the function fails - NULL.
+ * Otherwise - a pointer to the new string.
+ */
+
+char *argstostr(int ac, char **av)
+{
+	char *new_string = NULL;
+	int k = 0, i = ac, j, sum = 0, temp = 0;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	while (ac--)
+		sum += (len(av[ac]) + 1);
+	new_string = (char *) malloc(sum + 1);
+
+	if (new_string != NULL)
+	{
+		while (k < i)
+		{
+			for (j = 0; av[k][j] != '\0'; j++)
+				new_string[j + temp] = av[k][j];
+			new_string[temp + j] = '\n';
+			temp += (j + 1);
+			k++;
+		}
+		new_string[temp] = '\0';
+	}
+	else
+	{
+		return (NULL);
+	}
+	return (new_string);
+}
+
+/**
+ *len - returns length of str
+ *@str: string counted
+ *Return: returns the length
+ */
+int len(char *str)
+{
+	int len = 0;
+
+	if (str != NULL)
+	{
+		while (str[len])
+			len++;
+	}
+	return (len);
+}
---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,98 @@
+#include "main.h"
+
+/**
+ * strtow - splits a string into words
+ * @str: string that will be splitted
+ * Return: pointer to the array of splitted words
+ */
+
+char **strtow(char *str)
+{
+	char **split;
+	int i, j = 0, temp = 0, size = 0, words = count_words(str);
+
+	if (words == 0)
+		return (NULL);
+	split = (char **) malloc(sizeof(char *) * (words + 1));
+	if (split != NULL)
+	{
+		for (i = 0; i <= str_len(str) && words; i++)
+		{
+			if ((str[i] != ' ') && (str[i] != '\0'))
+				size++;
+			else if (((str[i] == ' ') || (str[i] == '\0')) && i && (str[i - 1] != ' '))
+			{
+				split[j] = (char *) malloc(sizeof(char) * size + 1);
+				if (split[j] != NULL)
+				{
+					while (temp < size)
+					{
+						split[j][temp] = str[(i - size) + temp];
+						temp++;
+					}
+					split[j][temp] = '\0';
+					size = temp = 0;
+					j++;
+				}
+				else
+				{
+					while (j-- >= 0)
+						free(split[j]);
+					free(split);
+					return (NULL);
+				}
+			}
+		}
+		split[words] = NULL;
+		return (split);
+	}
+	else
+		return (NULL);
+}
+
+/**
+ * count_words - counts the number of words in a string
+ * @s: string to be used
+ *Return: number of words
+ */
+
+int count_words(char *s)
+{
+	int i = 0, words = 0;
+
+	while (i <= str_len(s))
+	{
+		if ((s[i] != ' ') && (s[i] != '\0'))
+		{
+			i++;
+		}
+		else if (((s[i] == ' ') || (s[i] == '\0')) && i && (s[i - 1] != ' '))
+		{
+			words += 1;
+			i++;
+		}
+		else
+		{
+			i++;
+		}
+	}
+	return (words);
+}
+
+/**
+ * str_len - returns length of string
+ * @s: string to be counted
+ * Return: length of the string
+ */
+
+int str_len(char *s)
+{
+	int len = 0;
+
+	if (s != NULL)
+	{
+		while (s[len])
+			len++;
+	}
+	return (len);
+}
